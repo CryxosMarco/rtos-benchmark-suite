66da6da4e9accabeec7a0fd253baaff5d983d34b
Marco <milenkovic@ibv-augsburg.de>
2025-01-22 10:25:51 +0100

    update an false #ifdef syntax

--- task_synchronization.c
+++ task_synchronization.c
@@ -26,7 +26,7 @@ MMI: Marco Milenkovic, IBV, Milenkovic@ibv-augsburg.de
 /*******************************************************************************
  * includes
  ******************************************************************************/
-#ifdef USING_FREERTOS || USING_THREADX
+#if defined(USING_FREERTOS) || defined(USING_THREADX)
 #include "ti_board_open_close.h"
 #include "ti_drivers_open_close.h"
 #endif
