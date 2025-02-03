3cbd4fccc3ee15f567a89cc1a9f691db85502c45
Marco <milenkovic@ibv-augsburg.de>
2025-02-03 09:08:46 +0100

    added new basic test to ensure PMU is working correctl. And to make measurments from.

---
+++ pmu_test.c
@@ -0,0 +1,55 @@
+/*[CR]**************************************************************************
+Copyright (c) 2024 IBV - Echtzeit- und Embedded GmbH & Co. KG
+SPDX-License-Identifier: Apache-2.0
+*/
+/*[FH]**************************************************************************
+PROJECT: MASTER THESIS
+MODULE: SYNCHRONISATION TEST
+CONTENTS: Short description of the file content
+*/
+/*[CL]**************************************************************************
+21-01-2025 MMI Initial creation of the file
+
+---
+MMI: Marco Milenkovic, IBV, Milenkovic@ibv-augsburg.de
+*/
+/*[MP]**************************************************************************
+ * Synchronistation Promblem example using tm_api.h
+ *
+ * Demonstrates:
+ *   - How to set up a simple "UART" usage protected by a mutex
+ *   - Creating/resuming tasks
+ *   - Printing output with printf (simulating UART output)
+ *
+ *
+ ******************************************************************************/
+/*******************************************************************************
+ * includes
+ ******************************************************************************/
+
+#include "tm_api.h"
+
+
+
+
+/********************************************************************************
+ * MAIN ENTRY POINT
+ ********************************************************************************/
+int main_pmu(void)
+{
+   printf("[Main] Starting PMU Test Test.\n");
+   tm_setup_pmu();
+
+   tm_pmu_profile_start("PMU_Test");
+   tm_thread_sleep(1);
+   tm_pmu_profile_end("PMU_Test");
+   tm_pmu_profile_print("PMU_Test");
+
+   /* In many RTOSes, tm_initialize() might not return. If it does here,
+    * we just print a message. */
+   printf("[Main] Finished Test.\n");
+
+   return 0;
+}
+
+/*[EOF]************************************************************************/
