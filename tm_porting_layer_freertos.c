aa70c223e8d933f28830e980262167427cb49ab4
Marco Milenkovic <milenkovic@ibv-augsburg.de>
2025-02-12 11:36:03 +0100

    fixed correct usage of voided parameter

--- tm_porting_layer_freertos.c
+++ tm_porting_layer_freertos.c
@@ -109,7 +109,7 @@ void tm_thread_relinquish(void)
 /* Terminates the current thread and removes it from the scheduler */
 void tm_thread_exit(int thread_id)
 {
-   void(thread_id);
+    (void) thread_id;
    // Ensure the function is called from a valid FreeRTOS task context
    if (xTaskGetCurrentTaskHandle() != NULL)
    {
